21527acdd6f84d4d1bf5b95dba602645f9a61253
saradoubleu <swomiloju@gmail.com>
2018-05-14 21:33:23 +0000

    created getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,17 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
+/*
+In the next step you will implement the function to use the request library to programmatically fetch the list of contributors via HTTPS for the given repo.
+
+*/
